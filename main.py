c8291c41d656a7119bffe1a534e880e310e9a0f1
Андрей Бурцев <bossbyrcev221@gmail.com>
2021-05-03 17:47:26 +0300

    redesign app

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ class App(Commands, QMainWindow, Help):
     def initUI(self):
 
         self.setWindowTitle('Голосовий асистент CVA')
-        self.setWindowIcon(QIcon('img/voice_icon.png'))
+        self.setWindowIcon(QIcon('img/voice-assistant.png'))
         self.setIconSize(QSize(20, 20))
         self.setFixedSize(520, 400)
-        self.setGeometry(300, 300, 520, 400)
-        self.setStyleSheet("background-image: url(img/background.png);")
+        self.setGeometry(300, 300, 574, 421)
+        self.setStyleSheet("background-image: url(img/back.jpg);")
 
         self.font = QFont()
         self.font.setFamily("Bahnschrift SemiBold")
@@ -37,7 +37,7 @@ class App(Commands, QMainWindow, Help):
         self.help_btn.setText('Допомога')
         self.help_btn.setIconSize(QSize(17, 17))
         self.help_btn.setShortcut('F2')
-        self.help_btn.setStyleSheet("color: #fff; border-radius: 10px")
+        self.help_btn.setStyleSheet("color: #02386E; border-radius: 10px")
         self.help_btn.setCursor(QCursor(Qt.WhatsThisCursor))
         self.help_btn.setFont(self.font)
         self.help_btn.setToolTip('Натисніть клавішу F1, щоб дізнатися про можливості асистента')
@@ -45,10 +45,11 @@ class App(Commands, QMainWindow, Help):
 
         self.btn = QPushButton(self)
         self.btn.setGeometry(230, 300, 55, 90)
-        self.btn.setIcon(QIcon('img/micro.png'))
-        self.btn.setIconSize(QSize(50, 80))
+        self.btn.setIcon(QIcon('img/microphone.png'))
+        self.btn.setIconSize(QSize(70, 100))
         self.btn.setShortcut('F1')
         self.btn.setStyleSheet("border-radius: 20px")
+        self.btn.setToolTip('Натисніть клавішу F2, щоб дізнатися про можливості асистента')
         self.btn.setCursor(QCursor(Qt.PointingHandCursor))
         self.btn.clicked.connect(self.record_and_recognize_audio)
 
